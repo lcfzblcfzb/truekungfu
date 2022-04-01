f5b6756f137d45d9b2fef662ce2be100ff80c0e1
lcfzblcfzb <370733656@qq.com>
2022-04-01 18:52:33 +0800

    补充下

--- FrameWork/Fight/Wu/wuxue/behavior_tree/fist/Do_Wandering.gd
+++ FrameWork/Fight/Wu/wuxue/behavior_tree/fist/Do_Wandering.gd
@@ -10,7 +10,5 @@ func _pre_tick(agent: Node, blackboard: Blackboard) -> void:
 	if fight_componnent.fightKinematicMovableObj.state == FightKinematicMovableObj.ActionState.Idle:
 		return 
 	var moving_direction = Vector2.ZERO
-	if fight_componnent.actionMng._current_action!=null:
-		push_warning(fight_componnent.actionMng._current_action.base_action as String)
-	
+
 	fight_componnent.fight_controller.emit_new_fight_motion_event(Glob.getPollObject(MoveEvent,[moving_direction,false,false]))
